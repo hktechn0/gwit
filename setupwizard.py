16689afcd00ad207377dc41b228ad7ddaae32ca0
Hirotaka Kawata <hktechno@hotmail.com>
2010-06-15 14:04:11 +0900

    bug fix: gtk.Builder changed

--- setupwizard.py
+++ setupwizard.py
@@ -8,7 +8,6 @@ pygtk.require('2.0')
 import gtk
 
 import twoauth
-from objects import GtkObjects
 
 class SetupWizard:
     ok = False
@@ -22,9 +21,9 @@ class SetupWizard:
             os.path.dirname(__file__), "setupwizard.glade")
         
         builder = gtk.Builder()
+        self.builder = builder
         builder.add_from_file(setupglade)
         builder.connect_signals(self)
-        self.obj = GtkObjects(builder.get_objects())
         
         self.oauth = twoauth.oauth(*self.keys)
         self.rtoken = self.oauth.request_token()
@@ -34,27 +33,30 @@ class SetupWizard:
         lbutt = gtk.LinkButton(self.authurl, "Please Allow This Application")
         lbutt.connect("clicked", self.show_and_enable_pin)
 
-        self.obj.table1.attach(lbutt, 1, 2, 0, 1)
-        
+        self.get("table1").attach(lbutt, 1, 2, 0, 1)
+    
     def main(self):
-        self.obj.window1.show_all()
+        self.get("window1").show_all()
         gtk.main()
     
     def close(self, widget):
         gtk.main_quit()
+
+    def get(self, name):
+        return self.builder.get_object(name)
 
     def show_and_enable_pin(self, widget):
         
         urldlg = gtk.MessageDialog(buttons = gtk.BUTTONS_OK, message_format = self.authurl)
         urldlg.connect("response", self.show_and_enable_pin_close)
         urldlg.run()
-        self.obj.entry1.set_sensitive(True)
+        self.get("entry1").set_sensitive(True)
 
     def show_and_enable_pin_close(self, dialog, response_id):
         dialog.destroy()
         
     def on_button1_clicked(self, widget):
-        pin = int(self.obj.entry1.get_text())
+        pin = int(self.get("entry1").get_text())
         
         try:
             token = self.oauth.access_token(self.rtoken, pin)
@@ -69,21 +71,21 @@ class SetupWizard:
         lbl = gtk.Label()
         lbl.set_markup("<b>%s</b>" % self.screen_name)
         
-        self.obj.table1.attach(lbl, 1, 2, 2, 3)
-        self.obj.table1.show_all()
+        self.get("table1").attach(lbl, 1, 2, 2, 3)
+        self.get("table1").show_all()
         
-        self.obj.button1.set_sensitive(False)
-        self.obj.entry1.set_sensitive(False)
-        self.obj.button3.set_sensitive(True)
+        self.get("button1").set_sensitive(False)
+        self.get("entry1").set_sensitive(False)
+        self.get("button3").set_sensitive(True)
 
     def on_button3_clicked(self, widget):
         self.ok = True
-        self.obj.window1.destroy()
+        self.get("window1").destroy()
         gtk.main_quit()
     
     def on_entry1_changed(self, widget):
         pin = widget.get_text()
         if len(pin) == 7 and pin.isdigit():
-            self.obj.button1.set_sensitive(True)
+            self.get("button1").set_sensitive(True)
         else:
-            self.obj.button1.set_sensitive(False)
+            self.get("button1").set_sensitive(False)
