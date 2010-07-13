d92dca47e0de467cb58bbb6498017d2ad870fe37
Hirotaka Kawata <info@techno-st.net>
2010-07-14 05:15:17 +0900

    Release: 0.9-beta

--- gwit/__init__.py
+++ gwit/__init__.py
@@ -1,5 +1,32 @@
-#!/usr/bin/env python
 #-*- coding: utf-8 -*-
+
+'''module constractor
+'''
+
+################################################################################
+#
+# Copyright (c) 2010 University of Tsukuba Linux User Group
+#
+# This file is part of "gwit".
+#
+# "gwit" is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# "gwit" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "gwit".  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+
+__version__ = '0.9-beta'
+__author__ = 'University of Tsukuba Linux User Group <staff@tsukuba-linux.org>'
 
 from main import Main
 from setupwizard import SetupWizard
